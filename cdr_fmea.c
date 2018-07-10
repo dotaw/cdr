3c3757032235014c6d5320d124b4723c42e9ae6a
wangwei <dotaw@qq.com>
2018-07-10 12:00:46 +0800

    2018-07-10

---
+++ cdr_fmea.c
@@ -0,0 +1,331 @@
+#include "cdr_include.h"
+
+
+/* 实时获取记录仪的运行时间 */
+void cdr_get_recorder_runtime()
+{
+	cdr_get_system_time(CDR_TIME_S, g_dev_run_time.power_off_time[0]);
+	if (g_dev_run_time.num == 0)
+	{
+		g_dev_run_time.num = 1; /* 记录的设备数量，记录仪本身也算1个 */
+		g_dev_run_time.sa[0] = CDR_RECORDER_SA;
+	}
+	g_dev_run_time.is_update[0] = 1;
+}
+
+/* 获取硬盘当前使用状态，可用空间百分比=可用空间/总空间
+    1、硬盘空间剩余20%提示，黄灯闪烁
+    2、硬盘空间剩余15%告警，红灯闪烁，此时会覆盖部分数据
+    3、硬盘空间剩余2%告警， 红灯闪烁，此时会覆盖所有数据
+ */
+void cdr_fmea_disk_free_size()
+{
+    int size_total;
+    int size_free;
+    int state;
+    
+    size_total = cdr_get_disk_size_total(); /* 硬盘总空间 */
+    size_free  = cdr_get_disk_size_free();  /* 硬盘可用空间 */
+    
+    if (size_total == 0)
+    {    
+        g_system_event_occur[CDR_EVENT_STORAGE_NULL] = 1; /* 获取失败，上报严重告警 */
+        return;
+    }
+    
+    state = (size_free * 100) / size_total;
+	//cdr_diag_log(CDR_LOG_DEBUG, "The cdr_fmea_disk_free_size  size_total %u size_free %u state%u", size_total, size_free, state);
+	
+    if (state < CDR_STORAGE_NULL_THRESHOLD) /* 不足2% */
+    {
+        g_system_event_occur[CDR_EVENT_STORAGE_NULL] = 1; 
+    }    
+    else if (state < CDR_STORAGE_ALARM_THRESHOLD) /* 不足15% */
+    {
+        g_system_event_occur[CDR_EVENT_STORAGE_NULL] = 0;
+		g_system_event_occur[CDR_EVENT_STORAGE_ALARM] = 1;		
+    }    
+    else if(state < CDR_STORAGE_WARNING_THRESHOLD) /* 不足20% */
+    {
+		g_system_event_occur[CDR_EVENT_STORAGE_NULL] = 0;
+		g_system_event_occur[CDR_EVENT_STORAGE_ALARM] = 0;
+        g_system_event_occur[CDR_EVENT_STORAGE_WARNING] = 1;		
+    }
+    else
+    {
+		/* 存储空间正常，数据正常存储 */
+		g_system_event_occur[CDR_EVENT_STORAGE_NULL] = 0;
+		g_system_event_occur[CDR_EVENT_STORAGE_ALARM] = 0;
+		g_system_event_occur[CDR_EVENT_STORAGE_WARNING] = 0;		
+    }
+
+    return;
+}
+
+/* 设置LED状态灯的时候，需要根据优先级综合判断，最终的LED状态设置 */
+void cdr_fmea_set_led_state(int led_state)
+{
+    int i;
+    int led[] = 
+    {
+        /* 优先级排列，越前面，优先级别越高 */
+        CDR_LED_RED_CONTINUOUS, 
+        CDR_LED_RED_FLASH, 
+        CDR_LED_YELLOW_CONTINUOUS, 
+        CDR_LED_YELLOW_FLASH,
+        CDR_LED_GREEN_FLASH, 
+        CDR_LED_GREEN_CONTINUOUS,
+    }; 
+    
+	/* 没有数据正常运行的时候，状态为绿灯常亮 */
+	if (led_state == 0)
+	{
+		cdr_set_led_state(CDR_LED_GREEN_CONTINUOUS);
+		return;		
+	}
+	
+    for (i = 0; i < (sizeof(led) / sizeof(led[0])); i++)
+    {
+        if (led_state & led[i])
+        {
+            cdr_set_led_state(led[i]);
+            return;
+        }
+    }
+    
+    return;
+}
+
+void cdr_fmea_system_event_led_proc()
+{
+    int i;
+    int event;
+    int led_state = 0;
+    int led_info[CDR_EVENT_MAX][2] = 
+    {
+        {CDR_EVENT_FILE_RECORD_FAULT,      CDR_LED_RED_FLASH},
+        {CDR_EVENT_MYSQL_RECORD_FAULT,     CDR_LED_YELLOW_FLASH},
+        {CDR_EVENT_STORAGE_ALARM,          CDR_LED_RED_FLASH},
+        {CDR_EVENT_STORAGE_WARNING,        CDR_LED_YELLOW_FLASH},
+        {CDR_EVENT_DATA_RECORDING,         CDR_LED_GREEN_FLASH},
+		{CDR_EVENT_STORAGE_NULL,           CDR_LED_RED_FLASH},
+    };
+    
+    for (i = 0; i < CDR_EVENT_MAX - CDR_EVENT_FILE_RECORD_FAULT; i++)
+    {
+        event = led_info[i][0];
+        cdr_diag_log(CDR_LOG_DEBUG, "cdr_fmea_system_event_led_proc event %u fault %u", event, g_system_event_occur[event]);
+		
+        /* 故障发生 */
+        if (g_system_event_occur[event])
+        {
+            if (!g_system_event_occur_his[event]) /* 历史无故障 */
+            {
+                if (event != CDR_EVENT_DATA_RECORDING)
+                {
+                    cdr_diag_log(CDR_LOG_ERROR, "cdr_fmea_system_event_led_proc fault %u occurrence", event); /* 第一次发生，并且事件不是CDR_EVENT_DATA_RECORDING，记录日志 */
+					cdr_user_log(event, CDR_ERROR); /* 记用户日志 */
+                }
+				else
+				{
+					cdr_diag_log(CDR_LOG_DEBUG, "cdr_fmea_system_event_led_proc %u occurrence", event);
+				}
+                g_system_event_occur_his[event] = g_system_event_occur[event];
+            }
+            led_state |= led_info[i][1];
+			g_system_event_no_occur_num[event] = 0;
+        }
+        
+        /* 无故障 */
+        if (!g_system_event_occur[event])
+        {
+			/* 连续无故障次数超过3次，才进行恢复日志的打印，防止海量日志  */
+			g_system_event_no_occur_num[event]++;
+			if (g_system_event_no_occur_num[event] < CDR_EVENT_RECOVER_THRESHOLD)
+			{
+				led_state = g_led_state.state; /* 状态不变 */
+				continue;
+			}
+			
+            if (g_system_event_occur_his[event]) /* 历史有故障 */
+            {    
+                if (event != CDR_EVENT_DATA_RECORDING)
+                {
+                    cdr_diag_log(CDR_LOG_INFO, "cdr_fmea_system_event_led_proc fault %u recover", event); /* 故障恢复，并且事件不是CDR_EVENT_DATA_RECORDING，记录日志 */
+					cdr_user_log(event, CDR_OK);   /* 记用户日志 */		
+                }
+				else
+				{
+					cdr_diag_log(CDR_LOG_DEBUG, "cdr_fmea_system_event_led_proc %u no data input", event);
+				}
+                g_system_event_occur_his[event] = g_system_event_occur[event];
+            }
+        }
+    }
+    
+    cdr_fmea_set_led_state(led_state);
+    return;
+}
+
+/* 故障模拟处理 */
+void cdr_fmea_fault_sim_proc()
+{
+    FILE *fp;
+	char fault_sim[6] = {0};
+	int i;
+
+	if (access("/opt/myapp/test/fault_data_sim", F_OK) != 0) /* 文件不存在 */
+	{
+		return;
+	}
+	
+	if ((fp = fopen("/opt/myapp/test/fault_data_sim", "r")) == NULL) /* 文件打开失败 */
+	{
+		return;
+	}
+	
+	fgets(fault_sim, sizeof(fault_sim), fp);
+	if (fault_sim[0] == '\0')
+	{
+		return;
+	}
+	
+	cdr_diag_log(CDR_LOG_DEBUG, "cdr_fmea_fault_sim_proc fault_sim %s", fault_sim);
+	
+	for (i = 0; i < 5; i++)
+	{
+		g_system_event_occur[i + CDR_USR_LOG_TYPE_FILE_RECORD_FAULT] = 0;
+		if (fault_sim[i] == '2') /* 模拟状态 */
+		{
+			g_system_event_occur[i + CDR_USR_LOG_TYPE_FILE_RECORD_FAULT] = 1;
+		}
+	}
+
+	fclose(fp);
+	return;
+}
+
+/* 1s定时器 */
+void cdr_fmea_1s_timer()
+{
+    cdr_fmea_disk_free_size();	
+	cdr_fmea_fault_sim_proc();	
+    cdr_fmea_system_event_led_proc();
+    cdr_get_recorder_runtime();
+    
+    return;
+}
+
+/* 10s定时器 */
+void cdr_fmea_10s_timer()
+{	
+	/* 数据记录和数据存储线程正常，喂狗 */
+	cdr_diag_log(CDR_LOG_DEBUG, "cdr_fema g_pthread_record_data_active=%u, g_pthread_data_to_mysql_active=%u", g_pthread_record_data_active, g_pthread_data_to_mysql_active); 
+	if (g_pthread_record_data_active && g_pthread_data_to_mysql_active)
+	{
+		write(g_wdt_fd, "\0", 1);/* 喂狗 */
+		g_pthread_record_data_active = 0;
+		g_pthread_data_to_mysql_active = 0;
+		cdr_diag_log(CDR_LOG_DEBUG, "cdr_fema fedd watch dog is ok"); 
+	}
+
+    return;
+}
+
+/* ---------------------------------------- 文件对外函数 ---------------------------------------- */
+int cdr_record_self_test()
+{
+	int test_data[] = {0x555, 0xaaa};
+	int i;
+	FILE *fp;
+	char str_line[1000] = {0};
+	char table_info[500] = {0};
+	MYSQL_RES *result;
+    MYSQL_ROW sql_row;
+	
+	/* 创建数据表 */
+	memset(table_info, 0 , sizeof(table_info));
+	sprintf(table_info, "CREATE TABLE IF NOT EXISTS %s %s;", CDR_DATA_TABLE_SELF_TEST, CDR_DATA_TABLE_HEAD_SELF_TEST);
+	mysql_query(g_mysql_conn, table_info);
+	
+	for (i = 0; i < sizeof(test_data) / sizeof(test_data[0]); i++)
+	{
+		/* 数据写入文件 */
+		if ((fp = fopen("/opt/myapp/cdr_recorder/self_test.txt","w")) == NULL)
+		{
+			cdr_diag_log(CDR_LOG_ERROR, "The file %s can not be opened", "cache.0");
+			return CDR_ERROR;
+		}		
+		fprintf(fp, "'%u'", test_data[i]);
+		fclose(fp);
+		
+		/* 清空数据库 */
+		memset(table_info, 0 , sizeof(table_info));
+		sprintf(table_info, "truncate table %s;", CDR_DATA_TABLE_SELF_TEST);
+		mysql_query(g_mysql_conn, table_info);
+		
+		/* 数据通过文件写入mysql数据库 */
+		fp = fopen("/opt/myapp/cdr_recorder/self_test.txt","r");
+		fgets(str_line, 1000, fp);
+		memset(table_info, 0 , sizeof(table_info));
+		sprintf(table_info, "INSERT INTO %s(Data_Value) VALUES(%s);", CDR_DATA_TABLE_SELF_TEST, str_line);
+		if (mysql_query(g_mysql_conn, table_info) != 0)  /* 表格插入数据 */
+		{  
+			cdr_diag_log(CDR_LOG_ERROR, "cdr_record_self_test file to mysql fail info:%s , err:%s", table_info, mysql_error(g_mysql_conn));
+			return CDR_ERROR;
+		}
+		fclose(fp);
+		
+		/* 读取mysql数据库的值 */
+		memset(table_info, 0 , sizeof(table_info));
+		sprintf(table_info, "select * from %s", CDR_DATA_TABLE_SELF_TEST);
+		if (mysql_query(g_mysql_conn, table_info) != 0) /* 查询数据 */
+		{  
+			cdr_diag_log(CDR_LOG_ERROR, "cdr_record_self_test read mysql fail info:%s , err:%s", table_info, mysql_error(g_mysql_conn));
+			return CDR_ERROR;
+		}
+		result = mysql_store_result(g_mysql_conn); /* 保存查询到的数据到result */
+		sql_row = mysql_fetch_row(result); /* 获取值 */
+		
+		/* 结果对比，写入数据和读取数据一致，测试成功，否则失败 */
+		if (atoi(sql_row[1]) != test_data[i])
+		{
+			cdr_diag_log(CDR_LOG_ERROR, "cdr_record_self_test fail write data:%u , read data:%s", test_data[i], sql_row[1]);
+			mysql_free_result(result);
+			return CDR_ERROR;
+		}
+		cdr_diag_log(CDR_LOG_INFO, "cdr_record_self_test ok write data:%u , read data:%s", test_data[i], sql_row[1]);
+		mysql_free_result(result); /* 释放结果资源 */
+	}
+	
+	/* 删除文件，删除数据表 */
+	remove("/opt/myapp/cdr_recorder/self_test.txt");
+	memset(table_info, 0 , sizeof(table_info));
+	sprintf(table_info, "drop table %s", CDR_DATA_TABLE_SELF_TEST);
+	mysql_query(g_mysql_conn, table_info);	
+	
+    return CDR_OK;
+}
+
+void cdr_fmea_test()
+{
+    int i = 0;
+	
+	cdr_diag_log(CDR_LOG_INFO, "cdr_fmea_test >>>>>>>>>>>>>>>>>>>>>>>>>>in");
+
+    while (1) 
+    {
+        sleep(1); /* 延时1s */
+        
+        cdr_fmea_1s_timer();
+        
+        i++;
+        if (i >= 10)
+        {
+            cdr_fmea_10s_timer();
+            i = 0;
+        }
+    }
+    
+    return;
+}
---
+++ cdr_include.h
@@ -0,0 +1,213 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/reboot.h>
+#include <sys/statfs.h>
+#include "my_global.h"
+#include "my_sys.h"
+#include "mysql.h"
+
+#include <sys/ioctl.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <linux/socket.h>
+#include <linux/can.h>
+#include <linux/can/error.h>
+#include <linux/can/raw.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <linux/watchdog.h>
+
+
+/* ---------------------------------------- 宏定义 ---------------------------------------- */
+
+#define CDR_DEBUG_INVALID    0         /* 是否调试无效，用于日志的控制打印，软件最后发布时要定义成1 */
+
+#define CDR_FAIL_TRY_TIMES               3   /* 失败最大尝试次数 */
+#define CDR_EVENT_RECOVER_THRESHOLD      3   /* 恢复门限：连续无故障次数 */ 
+#define CDR_WATCH_DOG_THRESHOLD          60  /* 喂狗门限，超过门限不喂狗，系统复位，单位s */ 
+
+#define CDR_FILE_DIR_MAX_SIZE_BYTE       1000000   /* 1M */
+#define CDR_CACHE0_MAX_SIZE_BYTE         1000      /* 最大缓存文件大小，1k */
+
+#define CDR_DEV_RUNTIME_MAX_NUM          100    /* 能够同时记录运行时间的最大采集设备数量 */
+#define CDR_DIR_BF_MAX_NUM               20      /* 备份文件的最大数量 */
+
+#define CDR_RECORDER_SA                         0x59   /* 记录仪SA标识 */
+#define CDR_CAN_DATA_TIME_CALIBRATION_PF        0x6f   /* 时间校准PF标识 */
+#define CDR_CAN_DATA_HEART_BEAT_PF              0x39   /* 心跳报文PF标识 */
+
+#define CDR_OK      0  /* 正常返回值 */
+#define CDR_ERROR   1  /* 错误返回值 */
+
+#define CDR_STORAGE_WARNING_THRESHOLD    20    /* 硬盘空间剩余20%提示 */
+#define CDR_STORAGE_ALARM_THRESHOLD      10    /* 硬盘空间剩余10%告警，部分数据覆盖存储 */
+#define CDR_STORAGE_NULL_THRESHOLD       2     /* 硬盘空间剩余2%告警，所有的数据都要覆盖存储 */
+
+#define CDR_DEV_RUNNING_TIME_THRESHOLD   5   /* 超过门限没有坚持到设备心跳，认为设备关机，单位秒 */
+
+#define CDR_ID_TO_PRIORITY(id)     (((id) >> 26) & 0x7)    /* 根据CAN ID解析PRIORITY */
+#define CDR_ID_TO_RSV(id)          (((id) >> 24) & 0x3)    /* 根据CAN ID解析RSV */
+#define CDR_ID_TO_PF(id)           (((id) >> 16) & 0xff)   /* 根据CAN ID解析PF */
+#define CDR_ID_TO_PS_DA(id)        (((id) >> 8) & 0xff)    /* 根据CAN ID解析PS/DA */
+#define CDR_ID_TO_SA(id)           ((id) & 0xff)           /* 根据CAN ID解析SA */
+
+#define CDR_FILE_DIR_LOG           "/opt/myapp/cdr_recorder/log/"           /* 用户日志打印目录，需要记录到mysql数据库 */
+#define CDR_FILE_DIR_DIAGLOG       "/opt/myapp/cdr_recorder/diag/"          /* 诊断日志打印目录，用于问题的定位 */
+#define CDR_FILE_DIR_DIAGLOG_BF    "/opt/myapp/cdr_recorder/diag/bf/"       /* 诊断日志的缓存目录，数量有最大门限CDR_DIR_BF_MAX_NUM */
+#define CDR_FILE_DIR_CAN           "/opt/myapp/cdr_recorder/file/"          /* 文件记录的目录 */
+#define CDR_FILE_DIR_CAN_BF        "/opt/myapp/cdr_recorder/file/bf/"       /* 文件记录的备份目录，数量有最大门限CDR_DIR_BF_MAX_NUM */
+
+#define CDR_DATA_TABLE_PF41           "PF_41"         /* 存储PF等于0x41的数据 */
+#define CDR_DATA_TABLE_PF42           "PF_42"         /* 存储PF等于0x42的数据 */
+#define CDR_DATA_TABLE_PF_ELSE        "PF_ELSE"       /* 存储PF非0x41或0x42的数据 */
+#define CDR_DATA_TABLE_RUN_TIME       "RUN_TIME"      /* 存储设备的运行时间 */
+#define CDR_DATA_TABLE_USER_LOG       "USER_LOG"      /* 存储用户日志的记录 */
+#define CDR_DATA_TABLE_EVENT_TYPE     "EVENT_TYPE"    /* 存储用户日志中事件类型的说明 */
+#define CDR_DATA_TABLE_SELF_TEST      "SELF_TEST"     /* 临时表格，用户启动自检，启动完后表格删除 */
+
+/* 不同的表格对应的表头内容 */
+#define CDR_DATA_TABLE_HEAD_PF        \
+"(Serial INT(11) PRIMARY KEY AUTO_INCREMENT, PF VARCHAR(2), Time VARCHAR(23), Priority VARCHAR(2), Rsv VARCHAR(2), PSorDA VARCHAR(2), SA VARCHAR(2), Data VARCHAR(16), Data_Len  VARCHAR(2))"
+
+#define CDR_DATA_TABLE_HEAD_RUNTIME   \
+"(Serial INT(11) PRIMARY KEY AUTO_INCREMENT, SA VARCHAR(2), PowerOn_Time DATETIME, PowerOff_Time DATETIME, Running INT(11))"
+
+#define CDR_DATA_TABLE_HEAD_USER_LOG  \
+"(Serial INT(11) PRIMARY KEY AUTO_INCREMENT, Time VARCHAR(23), Event_Type VARCHAR(2), Result VARCHAR(5))"
+
+#define CDR_DATA_TABLE_HEAD_EVENT_TYPE  \
+"(Serial INT(11) PRIMARY KEY AUTO_INCREMENT, Event_Type VARCHAR(1), Event VARCHAR(30))"
+
+#define CDR_DATA_TABLE_HEAD_SELF_TEST  \
+"(Serial INT(11) PRIMARY KEY AUTO_INCREMENT, Data_Value INT(11))"
+
+#ifndef AF_CAN
+#define AF_CAN 29
+#endif
+#ifndef PF_CAN
+#define PF_CAN AF_CAN
+#endif
+
+#define WDT "/dev/watchdog"    /* 看门狗程序位置 */
+
+/* ---------------------------------------- 结构体或枚举定义 ---------------------------------------- */
+typedef enum cdr_log {
+    CDR_LOG_INFO = 1,       /* 关键事件诊断日志记录 */ 
+    CDR_LOG_ERROR,          /* 错误事件诊断日志记录 */ 
+    CDR_LOG_DEBUG,          /* 调试诊断日志记录，CDR_DEBUG_INVALID置1日志将不会打印 */  
+} cdr_log_t;
+
+typedef enum cdr_time {
+    CDR_TIME_MS = 1,        /* 获取时间精度到ms */
+    CDR_TIME_S,             /* 获取时间精度到s */
+} cdr_time_t;
+
+typedef enum cdr_led_state {
+    CDR_LED_RED_CONTINUOUS      = 0x1,        /* 红灯常亮 */
+    CDR_LED_RED_FLASH           = 0x2,        /* 红灯闪烁 */
+    CDR_LED_YELLOW_CONTINUOUS   = 0x4,        /* 黄灯常亮 */
+    CDR_LED_YELLOW_FLASH        = 0x8,        /* 黄灯闪烁 */
+    CDR_LED_GREEN_FLASH         = 0x10,       /* 绿灯闪烁 */
+    CDR_LED_GREEN_CONTINUOUS    = 0x20,       /* 绿灯常亮 */    
+} cdr_led_state_t;
+
+typedef enum cdr_user_log_type {  
+    CDR_USR_LOG_TYPE_INIT_CAN_IF = 1,           /* 用户日志事件类型：1、CAN接口初始化 */
+    CDR_USR_LOG_TYPE_INIT_MYSQL,                /* 用户日志事件类型：2、MySQL初始化 */
+    CDR_USR_LOG_TYPE_INIT_RECORD_SELTTEST,      /* 用户日志事件类型：3、数据存储自检 */
+    CDR_USR_LOG_TYPE_INIT_PTHREAD,              /* 用户日志事件类型：4、线程启动 */
+    CDR_USR_LOG_TYPE_FILE_RECORD_FAULT,         /* 用户日志事件类型：5、文件存储记录 */
+    CDR_USR_LOG_TYPE_MYSQL_RECORD_FAULT,        /* 用户日志事件类型：6、数据库记录数据 */
+	CDR_USR_LOG_TYPE_STORAGE_WARNING,           /* 用户日志事件类型：7、存储空间不足提示 */
+	CDR_USR_LOG_TYPE_STORAGE_ALARM,             /* 用户日志事件类型：8、存储空间不足告警 */
+	CDR_USR_LOG_TYPE_STORAGE_NULL,              /* 用户日志事件类型：9、存储空间不足 */
+} cdr_user_log_type_t;
+
+typedef enum cdr_event {  
+    CDR_EVENT_FILE_RECORD_FAULT = CDR_USR_LOG_TYPE_FILE_RECORD_FAULT,    /* 5、文件记录事件 */
+    CDR_EVENT_MYSQL_RECORD_FAULT,                                        /* 6、数据库记录事件 */
+    CDR_EVENT_STORAGE_WARNING,                                           /* 7、存储空间不足提示事件 */
+    CDR_EVENT_STORAGE_ALARM,                                             /* 8、存储空间不足告警事件 */
+    CDR_EVENT_STORAGE_NULL,                                              /* 9、存储空间不足事件 */
+    CDR_EVENT_DATA_RECORDING,                                            /* 10、数据正常存储事件 */
+    CDR_EVENT_MAX,
+} cdr_event_t;
+
+typedef struct cdr_can_frame {
+    int id;             /* can帧id */
+    long long data;       /* can数据 */
+    int len;            /* can数据长度 */
+} cdr_can_frame_t;
+
+typedef struct cdr_dev_run_time {
+    int num;                                               /* 当前记录到设备运行总数 */                                 
+    int is_update[CDR_DEV_RUNTIME_MAX_NUM];                /* 是否需要更新设备的运行时间*/
+    int add_id[CDR_DEV_RUNTIME_MAX_NUM];                   /* 设备运行时间记录在数据库中的位置 */
+	int sa[CDR_DEV_RUNTIME_MAX_NUM];                       /* 当前设备的sa标识 */
+    char power_on_time[CDR_DEV_RUNTIME_MAX_NUM][28];       /* 当前设备的上电时间 */
+    char power_off_time[CDR_DEV_RUNTIME_MAX_NUM][28];      /* 当前设备的下电时间 */
+} cdr_dev_run_time_t;
+
+typedef struct cdr_led_set_state {
+    int state;                                             /* LED的状态 */
+	char set_red[4];                                       /* 红灯是否输出 */
+	char set_green[4];                                     /* 绿灯是否输出 */
+	char set_yellow[4];                                    /* 黄灯是否输出 */
+	int is_flash;                                          /* 灯是否闪烁 */
+} cdr_led_set_state_t;
+
+/* ---------------------------------------- 全局变量定义 ---------------------------------------- */
+int g_socket_fd;                                  /* 创建的套接字 */
+int g_cache1_file_busy;                           /* 缓存文件cache.1是否正在被操作 */
+int g_dev_time_calibration_busy;                  /* 是否正在进行时间校准 */
+int g_dev_run_time_record_busy;                   /* 设备运行时间是否正在记录 */
+int g_diaglog_record_busy;                        /* 诊断日志记录是否正在被操作，防止多线程重入 */
+int g_userlog_record_busy;                        /* 用户日志记录是否正在被操作，防止多线程重入 */
+int g_write_file_no_proc_times;                   /* 连续未收到can数据的次数 */ 
+int g_user_log_no_proc_times;                     /* 连续未记录用户日志的次数 */ 
+int g_system_event_occur[CDR_EVENT_MAX];          /* 系统事件是否发生，事件见列表cdr_event_t */
+int g_system_event_occur_his[CDR_EVENT_MAX];      /* 系统事件是否发生过，事件见列表cdr_event_t */
+int g_system_event_no_occur_num[CDR_EVENT_MAX];   /* 系统事件连续没有发生的次数，事件见列表cdr_event_t */
+int g_wdt_fd;                                     /* 看门狗 */
+int g_pthread_record_data_active;                 /* 数据记录线程是否正常运行 */  
+int g_pthread_data_to_mysql_active;               /* 数据存储线程是否正常运行 */          
+cdr_led_set_state_t g_led_state;                  /* LED状态灯当前状态 */
+cdr_dev_run_time_t g_dev_run_time;                /* 设备的运行时间记录 */
+MYSQL *g_mysql_conn;                              /* 代开的数据库id */
+ 
+
+/* ---------------------------------------- 函数声明 ---------------------------------------- */
+/* cdr_main */
+void cdr_global_init();
+int cdr_main_can_if_init();
+int cdr_main_mysql_init();
+int cdr_main_record_self_test();
+int cdr_creat_pthread_record_can_data(pthread_t *pthread_record_data);
+int cdr_creat_pthread_add_data_to_mysql(pthread_t *pthread_data_to_mysql);
+int cdr_creat_pthread_fmea_test(pthread_t *pthread_fmea);
+int cdr_main_init_watch_dog();
+
+/* cdr_public */
+void cdr_get_system_time(int type, char *time_info);
+unsigned long get_file_size(const char *file);
+int cdr_cpy_file(char *file_in, char *file_out);
+void cdr_diag_log(int type, const char *format, ...);
+void cdr_set_led_state(int type);
+void cdr_system_reboot();
+void cdr_led_control();
+
+/* file_record */
+void cdr_record_can_data();
+
+/* mysql_record */
+void cdr_add_data_to_mysql();
+void cdr_mysql_end();
+
+/* cdr_fmea */
+void cdr_fmea_test();
